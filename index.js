941aba9951c57773d0c288feb108b0882376a8d7
Randy Champagne <r-champagne@neiu.edu>
2016-05-18 00:35:01 -0400

    completed work on module index

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 'use strict';
 
-module.exports = {
-  //proposals: require('./routes/proposals')
-};
+let module 	= require('../dist/module'),
+	data 	= require('./data/data');
+
+
+exports = function saveUrl(userToken, URL) {
+	return module.saveUrl(userToken, URL, data)
+}
+
+
+exports = function saveUrl(userToken, URL) {
+	return module.saveUrl(userToken, URL, data);
+}
+
+
+exports = function getUrls(userToken) {
+	return module.getUrls(userToken, data);
+}
+
+
+exports = function removeUrl(userToken, URL) {
+	return module.removeUrl(userToken, URL, data);
+}
+
+
+exports = function getUsersByDomain(URL) {
+	return module.getUsersByDomain(URL, data);
+}
+
+
+exports = function getRecommendedUrls(userToken, URL) {
+	return module.getRecommendedUrls(userToken, URL, data);
+}
+
+
+
